18951bf194147d53fe7a21faee79cc713db89fa4
motivinline-spec <motivinline@gmail.com>
2026-05-04 22:55:48 +0530

    2

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,22 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: './', // This ensures all paths are relative to your folder
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  }
 });
